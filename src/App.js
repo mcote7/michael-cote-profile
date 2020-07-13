b4e44db7851fb5adab9cfb614eb6501534e3e1d6
Michael Cote <michaelcote7@outlook.com>
2020-07-13 16:43:18 -0700

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React, {Component} from 'react';
 import {Route, Redirect, Switch} from 'react-router-dom';
 import scrollTop from './components/scrollTop';
-import NotFound from './components/notFound';
 import NavBar from './components/navigation';
 import MainPage from './components/mainPage';
 import Education from './components/education/education';
@@ -24,11 +23,9 @@ class App extends Component {
             <Route exact path="/education" component={Education}/>
             <Route exact path="/projects" component={Projects}/>
             <Route exact path="/resume" component={Resume}/>
-            <Route exact path="/not-found" component={NotFound}/>
             <Route exact path="/michael-cote" component={scrollTop}/>
             <Redirect from="/" exact to="/michael-cote"/>
-            <Redirect from="mcote7.github.io" exact to="/michael-cote"/>
-            <Redirect to="/not-found"/>
+            <Redirect to="/michael-cote"/>
           </Switch>
         </div>
       </div>
