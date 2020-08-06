0289e0c18000a35221c802da28c4fbbe82106c02
Michael Cote <michaelcote7@outlook.com>
2020-08-06 01:12:30 -0700

    .

--- src/utilities/onRoute.js
+++ src/utilities/onRoute.js
@@ -2,7 +2,11 @@ export function onRoute() {
   const cote = document.getElementById("cote").scrollHeight;
   // console.log("scroll height",cote)
   window.scrollTo({top: 0});
+
+  // window.scrollTo({top: cote, behavior: 'smooth'});
+
   setTimeout(()=> {
     window.scrollTo({top: cote, behavior: 'smooth'});
   }, 500);
+
 }
