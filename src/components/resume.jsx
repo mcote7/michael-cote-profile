0e2a573dc409358bebe1b7833c0be70fcdf8e5cc
Michael Cote <michaelcote7@outlook.com>
2020-08-16 23:50:38 -0700

    .

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -3,18 +3,51 @@ import { onRoute } from './../utilities/onRoute';
 import resume1 from '../images/resume1.jpg';
 import resume2 from '../images/resume2.jpg';
 import '../css/resume.css';
+import Typewriter from 'typewriter-effect';
 
 const Resume = () => {
   useEffect(() => {
     onRoute();
   },[])
   return (
-    <div className="container-fluid resumeContainer">
+    <>
+  <div className="allTypeCont mx-auto">
+    <div className="typeContainer">
+      <Typewriter
+        options={{
+          strings: ["I love making beautiful user experiences with JavaScript, Html, CSS and React"],
+          autoStart: true,
+          loop: true,
+          cursor: "_",
+          deleteSpeed: 10}}/>
+    </div>
+    <div className="typeContainer">
+      <Typewriter
+        options={{
+          strings: ["Fast load times and lag free interaction, my highest priority. . ."],
+          autoStart: true,
+          loop: true,
+          cursor: "_",
+          deleteSpeed: 10}}/>
+    </div>
+      <div className="typeContainer">
+      <Typewriter
+        options={{
+          strings: ["My responsive layouts will work on any device, big or small. . ."],
+          autoStart: true,
+          loop: true,
+          cursor: "_",
+          deleteSpeed: 10}}/>
+    </div>
+  </div>
+
+      <div className="container-fluid resumeContainer">
       <div className="col-xl-12 p-0 imageContainer">
         <img src={resume1} className="resumeImg" alt="img"/>
         <img src={resume2} className="resumeImg yelly" alt="img"/>
       </div>
     </div>
+    </>
   );
 }
 export default Resume;
