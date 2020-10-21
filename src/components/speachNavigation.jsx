afb16a7c2b3738b7fb41b93f9efd9823c0b30dd0
Michael Cote <michaelcote7@outlook.com>
2020-10-21 00:18:50 -0700

    .

--- src/components/speachNavigation.jsx
+++ src/components/speachNavigation.jsx
@@ -4,22 +4,50 @@ import '../css/navbar.css';
 const SpeachNavigation = (props) => {
   // console.log("speach props",props);
 
-  const [micIcon, setMicIcon] = useState("microphone-slash");
+  const [showSpeachInfo, setShowSpeachInfo] = useState(false);
   const handleMouseOver = () => {
-    setMicIcon("microphone");
+    setShowSpeachInfo(true);
   };
   const handleMouseLeave = () => {
-    setMicIcon("microphone-slash")
+    setShowSpeachInfo(false);
   };
 
+  const [micIcon, setMicIcon] = useState("microphone-slash");
+  const handleMouseDown = () => {
+    setMicIcon("microphone");
+  };
+  const handleMouseUp = () => {
+    setMicIcon("microphone-slash");
+  };
 
 
   return (<>
-    <div className="speach" onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
-      <span className="micIcon"><i className={`fa fa-${micIcon} fa-2x`} aria-hidden="true"></i></span>
+    <div className="speach" 
+      onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}
+      onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+      <span><i className={`fa fa-${micIcon} fa-2x`} aria-hidden="true"></i></span>
     </div>
 
-    <div className="speachInfo">Voice activated navigation!</div>
+    {showSpeachInfo ?
+    
+    <div className="speachInfo">
+
+      <span style={{fontSize: 'x-small'}}>{`( under construction )`}</span>
+      <h5><span role="img" aria-label="under-construction">&#128679; &#128679; &#128679;</span></h5>
+
+      <h5 style={{borderBottom: '2px solid'}}>Voice activated navigation!</h5>
+      
+        <div>1. Hold down <span><i className="fa fa-microphone" aria-hidden="true"></i></span> button</div>
+        <p>2. Try Saying :</p>
+        
+        <div>"Technical"</div>
+        <div>"Education"</div>
+        <div>"Projects"</div>
+        <div>"Resume"</div>
+        <div>"Contact"</div>
+        
+    </div>:''}
+
   </>);
 };
 export default SpeachNavigation;
