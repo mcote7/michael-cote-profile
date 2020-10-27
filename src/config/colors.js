a5394a7e862d0b19c85c5ed18480c1d6592b0e89
Michael Cote <michaelcote7@outlook.com>
2020-10-27 15:33:13 -0700

    .

---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export const MAIN_COLORS = {
+  myBlue: 'hsl(193, 95%, 68%)',
+  myYellow: 'hsl(54, 100%, 67%)',
+  myRed: 'hsl(336, 78%, 43%)',
+  myGreen: 'hsl(170, 100%, 50%)',
+  myPurple: 'hsl(275, 100%, 48%)'
+};
