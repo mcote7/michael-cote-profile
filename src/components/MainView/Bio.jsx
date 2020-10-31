68ee4fb3c2e6ea5cb987466b4858558714b673be
Michael Cote <michaelcote7@outlook.com>
2020-10-31 01:20:50 -0700

    .

--- src/components/MainView/Bio.jsx
+++ src/components/MainView/Bio.jsx
@@ -4,11 +4,12 @@ const Bio = ({self, handleMouseMove, handleMouseOut}) => {
 
   return (
     <div className="row">
-      <div onMouseMove={(e)=>handleMouseMove(e)} onMouseOut={(e)=>handleMouseOut(e)} className="bioWrap col-md-9">
+      <div className="bioWrap col-md-9">
 
-        <img src={self} className="mySelf float-right rounded-circle" alt="self"/>
+        <img src={self} className="mySelf float-right" alt="self"/>
           
-          <h4 className="quote"> <span role="img" aria-label="cool">&#10024;</span> 
+          <h4 className="quote" onMouseMove={(e)=>handleMouseMove(e)} onMouseOut={(e)=>handleMouseOut(e)} >
+            <span role="img" aria-label="cool">&#10024;</span> 
             Dedicated and motivated professional seeking an opportunity to showcase my creative and 
             technical proficiency. My goal is to attain a position which will provide an opportunity 
             for growth and personal development, as well as the opportunity to expand my knowledge.
