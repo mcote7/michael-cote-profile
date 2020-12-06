c6f58563cc7b85263e945479d00a4dd74ef7956f
Michael Cote <michaelcote7@outlook.com>
2020-12-05 22:54:38 -0800

    .

--- src/utilities/navBarCollapseTransition.js
+++ src/utilities/navBarCollapseTransition.js
@@ -3,7 +3,7 @@ export const navBarCollapseTransition = () => {
   const navbar_Collapse = document.getElementById("navbarText");
   const navbar_Main = document.getElementById("myNavbar");
   
-  if(navbar_Main !== null && navbar_Collapse !== null && navbar_Collapse.classList.includes("show")) {
+  if(navbar_Main !== null && navbar_Collapse !== null && [...navbar_Collapse.classList].includes("show")) {
     navbar_Main.animate([
       {opacity: 1},
       {opacity: 1, filter: 'blur(0px)'},
