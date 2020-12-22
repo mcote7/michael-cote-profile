f16516b07b3319d594d88481793f15e51f3ac558
Michael Cote <michaelcote7@outlook.com>
2020-12-22 11:11:30 -0800

    .

--- src/App.js
+++ src/App.js
@@ -35,12 +35,12 @@ const App = ({history}) => {
         {backgroundColor: 'rgba(0,0,0,1)'},
         {backgroundColor: 'rgba(0,0,0,0.75)'},
         {backgroundColor: 'rgba(255,255,255,0.75)'},
-        {backgroundColor: 'rgba(0,255,0,0.75)'},
-        {backgroundColor: 'rgba(0,255,0,0.9)'},
-        {backgroundColor: 'rgba(0,255,0,0.75)'},
-        {backgroundColor: 'rgba(255,0,0,0.75)'},
-        {backgroundColor: 'rgba(255,0,0,0.9)'},
-        {backgroundColor: 'rgba(255,0,0,0.75)'},
+        {backgroundColor: 'rgba(0,200,0,0.75)'},
+        {backgroundColor: 'rgba(0,200,0,1)'},
+        {backgroundColor: 'rgba(0,200,0,0.75)'},
+        {backgroundColor: 'rgba(200,0,0,0.75)'},
+        {backgroundColor: 'rgba(200,0,0,1)'},
+        {backgroundColor: 'rgba(200,0,0,0.75)'},
         {backgroundColor: 'rgba(0,0,0,0.75)'},
         {backgroundColor: 'rgba(0,0,0,1)'}
       ], {
