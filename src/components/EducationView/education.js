c2e1f59fbfbc44ffca147f78fc4b8db089913048
Michael Cote <michaelcote7@outlook.com>
2021-05-11 22:05:56 -0700

    .

--- src/components/EducationView/education.js
+++ src/components/EducationView/education.js
@@ -4,35 +4,40 @@ export const EDU = [
     id: 1, 
     image: 'dojo-img', 
     info: 'Completed intensive full-time software development program. Developed multiple full-stack projects in ASP.Net, Python/Django & MERN.', 
-    logo: 'dojo-logo',
-    speach: 'coding dojo'
+    logo: 'dojo-logo', 
+    date: 'June 2020', 
+    speach: 'coding dojo' 
   },
   {
     id: 2, 
     image: 'angular-img', 
     info: 'Completed course in Angular4, a deep dive into Angular’s core modules and integration with Firebase.', 
-    logo: 'mosh-logo',
-    speach: 'angular'
+    logo: 'mosh-logo', 
+    date: 'May 2021', 
+    speach: 'angular' 
   },
   {
     id: 3, 
     image: 'sass-img', 
     info: 'Completed course in SASS workflow, taking my stylesheets to the next level.', 
-    logo: 'udemy-logo',
-    speach: 'super awesome style sheets'
+    logo: 'udemy-logo', 
+    date: 'September 2021', 
+    speach: 'super awesome style sheets' 
   },
   {
     id: 4, 
     image: 'react-img', 
     info: 'Completed course in React library, gained knowledge of both legacy ‘class component’ structure and the new ‘functional component’ structure along with MongoDb integration.', 
-    logo: 'mosh-logo',
-    speach: 'react'
+    logo: 'mosh-logo', 
+    date: 'July 2020', 
+    speach: 'react' 
   },
   {
     id: 5, 
     image: 'redux-img', 
     info: 'Completed course in Redux state management and its intergration with React.', 
-    logo: 'mosh-logo',
-    speach: 'redux'
+    logo: 'mosh-logo', 
+    date: 'August 2020', 
+    speach: 'redux' 
   }
 ];
