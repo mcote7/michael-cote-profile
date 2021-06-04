101f0a58d9957d16435995b0bd063bd129777588
Michael Cote <michaelcote7@outlook.com>
2021-06-04 01:01:30 -0700

    .

--- src/components/ProjectsView/ProjectsView.jsx
+++ src/components/ProjectsView/ProjectsView.jsx
@@ -1,20 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../SASS/projects-view.scss';
 import {onRoute} from '../../utilities/onRoute';
 
 import {PROJ} from '../../config/projects';
 import ProjectCard from '../ProjectsView/ProjectCard';
 
+import moment from 'moment';
+
 
 const ProjectsView = () => {
 
+  const [GIT, setGIT] = useState();
+
   useEffect(()=> {
     onRoute();
-    fetch('https://api.github.com/users/mcote7/repos', {method: 'GET'})
+    fetch('https://api.github.com/users/mcote7/repos?page=1&per_page=100', {method: 'GET'})
       .then(res => res.json())
-      .then(data => console.log("data", data))
+      .then(data => {
+        console.log("data", data)
+        setGIT(data);
+      });
   }, []);
 
+  async function getLanguages(e, languages_url, id) {
+    e.preventDefault();
+    let el = document.getElementById(id); 
+    await fetch(`${languages_url}`, {method: 'GET'})
+      .then(res => res.json())
+      .then(data => {
+        console.log("data", data) // works!!!
+        el.innerHTML = data;
+      });
+  }
 
   return ( <React.Fragment>
     <div className="pro-row row">
@@ -31,9 +48,39 @@ const ProjectsView = () => {
     </div>
     
     {/* more from git hub api */}
-    
-    <div className="row my-5" style={{height: '100vh', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <p>🚧 🏗 👷‍♂️</p>&nbsp;<p>in dev...</p>
+    <div className="git-row my-5">
+      {GIT && GIT.map(( repo, idx ) => {
+        return(
+          <div key={idx} className="col-xl-3 col-lg-4 col-md-6 git-col my-3">
+            <a 
+              id={`${idx}card`}
+              className="card git-card" 
+              href={`${repo.html_url}`} 
+              target="_blank" rel="noopener noreferrer">
+              
+              <div className="card-title">
+                <i className="fa fa-github git-logo" aria-hidden="true"></i>
+                <div><small>({idx})</small> <strong>{repo.name}</strong></div>
+              </div>
+              <div className="card-url">
+                <p>{repo.html_url}</p>
+              </div>
+              <div className="card-dates">
+                <small>updated: {moment(repo.updated_at).format('MMMM Do YYYY')}</small>
+                <small>created: {moment(repo.created_at).format('MMMM Do YYYY')}</small>
+              </div>
+              <div className="repo-languages">
+                <button 
+                  onClick={(e) => getLanguages(e, repo.languages_url, `${idx}lang`)} 
+                  className="btn btn-secondary">
+                  &lt;Languages/&gt;
+                </button>
+                <p id={`${idx}lang`}></p>
+              </div>
+            </a>
+          </div>
+        );
+      })}
     </div>
   </React.Fragment> );
 };
