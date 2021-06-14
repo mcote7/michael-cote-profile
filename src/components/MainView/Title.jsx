9b475b7cd62987a5064fa84ce2833f29883728f4
Michael Cote <michaelcote7@outlook.com>
2021-06-13 21:08:02 -0700

    .

--- src/components/MainView/Title.jsx
+++ src/components/MainView/Title.jsx
@@ -11,10 +11,13 @@ const Title = ({light}) => {
         </div>
         <div className="titleSub">
           <span style={{color: 'white', letterSpacing: 0}}>&#8943;&#8886;</span>
-          &nbsp;WEB DEVELOPER IN 2021
+          &nbsp;WEB DEVELOPER IN 2020
           <span style={{color: 'white', letterSpacing: 0}}>&#8887;&#8943;</span>
         </div>
       </div>
+      <a href="https://mcote7.github.io/michael-cote/" className="alert alert-danger" style={{position: 'sticky'}}>
+        This site is old, my first in fact. check out my new site <span><i class="fa fa-external-link" aria-hidden="true"></i></span>
+      </a>
     </div>
   </> 
   );
